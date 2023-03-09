03978d8f50b4a623e85385e95d8fe4fd5aa5456f
Fran <franjca12@gmail.com>
2023-03-09 14:21:11 -0500

    acvtualizando

---
+++ python/piedra papel tijera.py
@@ -0,0 +1,14 @@
+#piedra papel tijera
+#random 0-2  
+import random
+print("Programa piedra papel tijera vs cpu")
+
+#Respuesta = input("Ingresa 1.piedra 2.papel 3. tijera")
+cpu_R=("piedra","papel","tijera")
+#piedra gana tijeras 
+#
+cpu=random.randint(1,3)
+print(cpu)
+
+ppt={"piedra"+1:"gana", }
+
