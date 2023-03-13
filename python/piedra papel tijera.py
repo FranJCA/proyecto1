2ee6e96feb61e2e3f7c3fc17c542bc83a2977e08
Fran <franjca12@gmail.com>
2023-03-13 15:51:23 -0500

    listo

--- python/piedra papel tijera.py
+++ python/piedra papel tijera.py
@@ -4,11 +4,44 @@ import random
 print("Programa piedra papel tijera vs cpu")
 
 #Respuesta = input("Ingresa 1.piedra 2.papel 3. tijera")
-cpu_R=("piedra","papel","tijera")
-#piedra gana tijeras 
-#
-cpu=random.randint(1,3)
-print(cpu)
+variable=["piedra","papel","tijera"] 
 
-ppt={"piedra"+1:"gana", }
 
+while True:
+   # print("a")
+    try:
+        cpu1= int(input("ingrese un numero 1.piedra 2. papel 3. tijera\n"))
+        print(type(cpu1))
+        
+        if(cpu1>0 and cpu1<3):
+            cpu1= variable[cpu1 -1]
+            print(cpu1)
+            break
+        else:
+            print("fuera de rango ingrese una opcion valida")   
+    except:
+        print("ingresa una opcion valida")
+        
+        
+
+print(cpu1)
+#cpu1= variable[0]
+#cpu2= variable[1] 
+cpu2= random.choice(variable)
+campo={cpu1,cpu2}
+print(campo)
+campo.union(cpu1,cpu2)
+
+print("Usuario: "+ cpu1+" computadora: "+ cpu2)
+
+if(("piedra" and "tijera" )in campo )==True:
+    print( str(campo)  + "gana piedra")
+    
+elif(("piedra" and "papel" )in campo)==True:
+    print( str(campo)  + "gana papel")
+elif(("papel" and "tijera" )in campo)==True:
+    print( str(campo)  + "gana tijera")
+else:
+    print("empate")
+#hagalo sencillo  opcion 1 2 3
+#si escribe tijera = 1 
