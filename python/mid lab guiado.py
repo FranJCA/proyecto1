7cc2e93fc530c483beb655c4f88476596a6110a8
Fran <franjca12@gmail.com>
2023-05-02 14:44:15 -0500

    git

---
+++ python/mid lab guiado.py
@@ -0,0 +1,7 @@
+color= input("Di un color ")
+plurarl_noun = input("Enter a plural Noun: ")
+celebridad= input("Nombre de alguna celebridad ")
+
+print("las rosas son "+ color)
+print(plurarl_noun +" are blue")
+print("Te amo "+ celebridad)
